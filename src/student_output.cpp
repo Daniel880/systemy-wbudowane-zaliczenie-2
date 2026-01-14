f82d53f18a6f37e1553a19a8593270bef8675011
LukaszBlajchert <lukasz.blajchert@student.put.poznan.pl>
2026-01-14 16:07:44 +0000

    Fix output format

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 
 void generate_diagnostics_report() {
-	std::cout << "==System status==" << std::endl;
-	std::cout << "Engineer: " << "LukaszBlajchert" <<std::endl;
+	std::cout << "==SYSTEM STATUS==" << std::endl;
+	std::cout << "Engineer: LukaszBlajchert" <<std::endl;
 	std::cout << "Branch: " << get_branch_name() << std::endl;
 	std::cout << "Uptime: " <<get_system_uptime() << std::endl;
 	std::cout << "Running Processes: " << get_running_processes() << std::endl;
