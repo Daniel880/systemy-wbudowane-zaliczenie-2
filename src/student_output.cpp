5e7516ae02434c0eab97e1a9afb873fff39a7b90
WiktorFolerzynski <Wiktor@Folerzynski.com>
2026-01-14 15:57:43 +0000

    Wgranie poprawione zaliczenie 2 28.01.26 v3

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -7,5 +7,5 @@ std::cout << "=== SYSTEM STATUS ===" << std::endl;
 std::cout << "Engineer: " << "WiktorFolerzynski" <<std:: endl;
 std::cout << "Branch: " << get_branch_name() << std::endl;
 std::cout << "Uptime: " << get_system_uptime() << std::endl;
-std::cout << "Running Prosesses: " << get_running_processes() << std::endl;
+std::cout << "Running Processes: " << get_running_processes() << std::endl;
 }
