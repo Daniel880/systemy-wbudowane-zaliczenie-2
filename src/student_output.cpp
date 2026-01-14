2b4fa762b8f020d7cb071f411c67cd07d3a220b8
WiktorFolerzynski <Wiktor@Folerzynski.com>
2026-01-14 15:47:53 +0000

    Dodanie zadania do brancha

---
+++ src/student_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <diagnostics.h>
+#include <student_output.h>
+
+void generate_diagnostics_report(){
+std::cout << "===System Status===" << std::endl;
+std::cout << "Engineer: " << "WiktorFolerzynski" <<std:: endl;
+std::cout << "Branch: " << get_branch_name() << std::endl;
+std::cout << "Uptime: " << get_system_uptime() << std::endl;
+std::cout << "Running Prosesses: " << get_running_processes() << std::endl;
+}
