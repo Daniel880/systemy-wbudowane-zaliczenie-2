dc467773c0b9a7027a63f4e1c07a013a32bba093
WiktorFolerzynski <Wiktor@Folerzynski.com>
2026-01-14 15:54:50 +0000

    Wgranie zaliczenie 2 28.01.26

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -3,7 +3,7 @@
 #include <student_output.h>
 
 void generate_diagnostics_report(){
-std::cout << "===System Status===" << std::endl;
+std::cout << "=== SYSTEM STATUS ===" << std::endl;
 std::cout << "Engineer: " << "WiktorFolerzynski" <<std:: endl;
 std::cout << "Branch: " << get_branch_name() << std::endl;
 std::cout << "Uptime: " << get_system_uptime() << std::endl;
