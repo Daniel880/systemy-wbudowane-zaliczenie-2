f44b02b666ef9e017e3797758e687903963172dd
TomaszWalkiewicz <tomasz@walkiewicz.com>
2026-02-09 16:10:45 +0100

    cwiczenie obslugi git przed poprawa

---
+++ build/CMakeFiles/zaliczenie2.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/zaliczenie2.dir/src/main.cpp.o: \
+ /home/tomek/systemy-wbudowane-zaliczenie-2/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/tomek/systemy-wbudowane-zaliczenie-2/include/student_output.h
