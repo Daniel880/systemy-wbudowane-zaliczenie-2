d4129e15b410103e2955a9bdb26e908c2928af8d
Zuzanna Kozak <zuzanna@kozak.com>
2026-02-10 08:58:17 +0000

    zmiana student_output.cpp

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -1,13 +1,21 @@
 #include "student_output.h"
 #include "diagnostics.h"
+#include <string>
 
 #include <iostream>
 using namespace std;
 
 void generate_diagnostics_report(){
+string branch = get_branch_name();
+string prefix = "Zaliczenie2-";
+
+if(branch.find(prefix)==0){
+branch.erase(0,prefix.length());
+}
+
 cout<<"=== SYSTEM STATUS ==="<<endl;
 cout<<"Engineer: "<<"ZuzannaKozak"<<endl;
-cout<<"Branch :"<<get_branch_name()<<endl;
+cout<<"Branch :"<<branch<<endl;
 cout<<"Uptime: "<<get_system_uptime()<<endl;
 cout<<"Running Processes: "<<get_running_processes()<<endl;
 }
